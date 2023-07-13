b6d2807bde4039f3eef6c74b727aeb87b8dd8248
bonamsetty20 <bonamsettyjahnavi@gmail.com>
2023-07-13 14:48:49 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conappAssig5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Username = "Janu";
+            string Password = "28665";
+            int attemptsRemaining = 3;
+
+            while (attemptsRemaining > 0)
+            {
+                Console.Write("Enter username: ");
+                string enterusername = Console.ReadLine();
+
+                Console.Write("Enter password: ");
+                string enterpassword = Console.ReadLine();
+
+                if (enterusername == Username && enterpassword == Password)
+                {
+                    Console.WriteLine("Login successful.");
+                    return;
+                }
+                else if (enterusername == Username)
+                {
+                    Console.WriteLine("Invalid password.");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid username.");
+                }
+
+                attemptsRemaining--;
+            }
+
+            Console.WriteLine("Login failed.");
+        }
+    }
+}
